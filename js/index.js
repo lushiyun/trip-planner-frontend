a735a1adf4615466b9c0a01989d84fe385e66497
github username <lushiyun@outlook.com>
2020-08-12 19:48:39 -0400

    Add basic structure and map functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-require('dotenv').config()
+import { elements } from './modules/elements.js';
+import * as googleMap from './modules/googleMap.js';
+
+const state = {};
+
+googleMap.searchBox.addListener('places_changed', () => {
+  const city = googleMap.searchBox.getPlaces()[0];
+  if(city === null) return
+  state.mapCenter = city.geometry.location;
+});
+
+const submitInitForm = (e) => {
+  if (!state.mapCenter) {
+    console.log('something went wrong');
+  } else {
+    googleMap.initMap(state.mapCenter);
+    loadPlaces();
+    elements.initModal.style.display = 'none';
+  }
+  e.preventDefault();
+}
+
+async function loadPlaces () {
+  await googleMap.getPlaces(state.mapCenter, 12000, 'lodging');
+  googleMap.createMarker('lodging');
+  googleMap.createCard('lodging');
+}
+
+elements.initForm.addEventListener('submit', submitInitForm);
---
+++ js/modules/elements.js
@@ -0,0 +1,9 @@
+export const elements = {
+  map: document.querySelector('#map'),
+  cityInput: document.querySelector('#input-city'),
+  initForm: document.querySelector('#init-form'),
+  startDate: document.querySelector('#start-date'),
+  endDate: document.querySelector('#end-date'),
+  initModal: document.querySelector('#search-modal'),
+  placeList: document.querySelector('#place-list')
+}
